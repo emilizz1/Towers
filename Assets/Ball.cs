b2c7e44779f53c0ad3deeff888b2746575484a3c
Emilijus <slibinas2012@gmail.com>
2017-06-06 21:58:14 +0300

    It Moves!!

---
+++ Assets/Ball.cs
@@ -0,0 +1,45 @@
+using System;
+using UnityEngine;
+
+[RequireComponent(typeof(UnityEngine.AI.NavMeshAgent))]
+public class Ball : MonoBehaviour
+{
+    [SerializeField] private float m_MovePower = 5; // The force added to the ball to move it.
+
+    public UnityEngine.AI.NavMeshAgent agent { get; private set; }
+    public Transform target;
+    
+    private Rigidbody m_Rigidbody;
+
+
+    private void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+
+        agent = GetComponentInChildren<UnityEngine.AI.NavMeshAgent>();
+
+        agent.updateRotation = false;
+        agent.updatePosition = true;
+    }
+
+    private void Update()
+    {
+        if (target != null)
+            agent.SetDestination(target.position);
+
+        if (agent.remainingDistance > agent.stoppingDistance)
+            Move(agent.desiredVelocity);
+        else
+            Move(Vector3.zero);
+    }
+
+    public void SetTarget(Transform target)
+    {
+        this.target = target;
+    }
+
+    public void Move(Vector3 moveDirection)
+    {
+            m_Rigidbody.AddForce(moveDirection * m_MovePower);
+    }
+}
